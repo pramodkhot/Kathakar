b7cc399b88d05386676804ba678169eeeab2acf1
pramodkhot <44355634+pramodkhot@users.noreply.github.com>
2026-04-27 16:17:55 +0530

    Update KathakarNavGraph.kt

--- app/src/main/java/com/kathakar/app/navigation/KathakarNavGraph.kt
+++ app/src/main/java/com/kathakar/app/navigation/KathakarNavGraph.kt
@@ -24,12 +24,15 @@ sealed class Screen(val route: String) {
     object StoryDetail : Screen("story/{storyId}") {
         fun go(id: String) = "story/$id"
     }
-    object EpisodeReader : Screen("episode/{episodeId}") {
-        fun go(id: String) = "episode/$id"
+    object EpisodeReader : Screen("episode/{episodeId}/{authorId}/{storyId}") {
+        fun go(episodeId: String, authorId: String, storyId: String) =
+            "episode/$episodeId/$authorId/$storyId"
+    }
+    object EditEpisode : Screen("edit/episode/{episodeId}/{storyId}") {
+        fun go(episodeId: String, storyId: String) = "edit/episode/$episodeId/$storyId"
     }
 }
 
-// Clean tab switching - saves state, no back stack buildup
 fun NavHostController.switchTab(route: String) {
     navigate(route) {
         popUpTo(Screen.Home.route) { saveState = true }
@@ -43,10 +46,9 @@ fun KathakarNavGraph(navController: NavHostController) {
     val authVM: AuthViewModel = hiltViewModel()
     val auth by authVM.state.collectAsState()
 
-    NavHost(
-        navController    = navController,
-        startDestination = if (auth.isAuthenticated) Screen.Home.route else Screen.Login.route
-    ) {
+    NavHost(navController = navController,
+        startDestination = if (auth.isAuthenticated) Screen.Home.route else Screen.Login.route) {
+
         composable(Screen.Login.route) {
             LoginScreen(viewModel = authVM, onSuccess = {
                 navController.navigate(Screen.Home.route) {
@@ -57,62 +59,76 @@ fun KathakarNavGraph(navController: NavHostController) {
 
         composable(Screen.Home.route) {
             val user = auth.user ?: return@composable
-            HomeScreen(
-                user           = user,
+            HomeScreen(user = user,
                 onStoryClick   = { navController.navigate(Screen.StoryDetail.go(it)) },
                 onWriteClick   = { navController.switchTab(Screen.Write.route) },
                 onLibraryClick = { navController.switchTab(Screen.Library.route) },
-                onProfileClick = { navController.switchTab(Screen.Profile.route) }
-            )
+                onProfileClick = { navController.switchTab(Screen.Profile.route) })
         }
 
         composable(Screen.StoryDetail.route,
             listOf(navArgument("storyId") { type = NavType.StringType })) { back ->
             val storyId = back.arguments?.getString("storyId") ?: return@composable
             val user    = auth.user ?: return@composable
-            StoryDetailScreen(
-                storyId       = storyId,
-                user          = user,
+            StoryDetailScreen(storyId = storyId, user = user,
                 onBack        = { navController.popBackStack() },
-                onReadEpisode = { navController.navigate(Screen.EpisodeReader.go(it)) },
-                onBuyCoins    = { navController.navigate(Screen.BuyCoins.route) }
-            )
+                onReadEpisode = { episodeId, authorId ->
+                    navController.navigate(Screen.EpisodeReader.go(episodeId, authorId, storyId))
+                },
+                onBuyCoins    = { navController.navigate(Screen.BuyCoins.route) })
         }
 
-        composable(Screen.EpisodeReader.route,
-            listOf(navArgument("episodeId") { type = NavType.StringType })) { back ->
-            val epId = back.arguments?.getString("episodeId") ?: return@composable
+        composable(Screen.EpisodeReader.route, listOf(
+            navArgument("episodeId") { type = NavType.StringType },
+            navArgument("authorId")  { type = NavType.StringType },
+            navArgument("storyId")   { type = NavType.StringType }
+        )) { back ->
+            val episodeId = back.arguments?.getString("episodeId") ?: return@composable
+            val authorId  = back.arguments?.getString("authorId")  ?: return@composable
+            val storyId   = back.arguments?.getString("storyId")   ?: return@composable
+            val user      = auth.user ?: return@composable
             EpisodeReaderScreen(
-                episodeId = epId,
-                onBack    = { navController.popBackStack() }
-            )
+                episodeId      = episodeId,
+                storyId        = storyId,
+                authorId       = authorId,
+                currentUserId  = user.userId,
+                onBack         = { navController.popBackStack() },
+                onEdit         = { navController.navigate(Screen.EditEpisode.go(episodeId, storyId)) },
+                onDeleted      = { navController.popBackStack() })
+        }
+
+        composable(Screen.EditEpisode.route, listOf(
+            navArgument("episodeId") { type = NavType.StringType },
+            navArgument("storyId")   { type = NavType.StringType }
+        )) { back ->
+            val episodeId = back.arguments?.getString("episodeId") ?: return@composable
+            val storyId   = back.arguments?.getString("storyId")   ?: return@composable
+            EditEpisodeScreen(episodeId = episodeId, storyId = storyId,
+                onDone = { navController.popBackStack() },
+                onBack = { navController.popBackStack() })
         }
 
         composable(Screen.Write.route) {
             val user = auth.user ?: return@composable
-            WriteScreen(
-                user            = user,
+            WriteScreen(user = user,
                 onCreateStory   = { navController.navigate(Screen.CreateStory.route) },
                 onCreateEpisode = { sid, ch -> navController.navigate(Screen.CreateEpisode.go(sid, ch)) },
                 onAiClick       = { navController.navigate(Screen.AiWrite.route) },
                 onBack          = { navController.switchTab(Screen.Home.route) },
                 onReadStory     = { navController.navigate(Screen.StoryDetail.go(it)) },
                 onLibraryClick  = { navController.switchTab(Screen.Library.route) },
-                onProfileClick  = { navController.switchTab(Screen.Profile.route) }
-            )
+                onProfileClick  = { navController.switchTab(Screen.Profile.route) })
         }
 
         composable(Screen.CreateStory.route) {
             val user = auth.user ?: return@composable
-            CreateStoryScreen(
-                user    = user,
+            CreateStoryScreen(user = user,
                 onSaved = { sid ->
                     navController.navigate(Screen.CreateEpisode.go(sid, 1)) {
                         popUpTo(Screen.CreateStory.route) { inclusive = true }
                     }
                 },
-                onBack  = { navController.popBackStack() }
-            )
+                onBack = { navController.popBackStack() })
         }
 
         composable(Screen.CreateEpisode.route, listOf(
@@ -122,41 +138,31 @@ fun KathakarNavGraph(navController: NavHostController) {
             val storyId = back.arguments?.getString("storyId") ?: return@composable
             val ch      = back.arguments?.getInt("chNum") ?: 1
             val user    = auth.user ?: return@composable
-            CreateEpisodeScreen(
-                storyId       = storyId,
-                chapterNumber = ch,
-                authorId      = user.userId,
-                onDone        = { navController.popBackStack() },
-                onBack        = { navController.popBackStack() }
-            )
+            CreateEpisodeScreen(storyId = storyId, chapterNumber = ch, authorId = user.userId,
+                onDone = { navController.popBackStack() },
+                onBack = { navController.popBackStack() })
         }
 
         composable(Screen.Library.route) {
             val user = auth.user ?: return@composable
-            LibraryScreen(
-                userId         = user.userId,
+            LibraryScreen(userId = user.userId,
                 onStoryClick   = { navController.navigate(Screen.StoryDetail.go(it)) },
                 onBack         = { navController.switchTab(Screen.Home.route) },
                 onWriteClick   = { navController.switchTab(Screen.Write.route) },
-                onProfileClick = { navController.switchTab(Screen.Profile.route) }
-            )
+                onProfileClick = { navController.switchTab(Screen.Profile.route) })
         }
 
         composable(Screen.Profile.route) {
             val user = auth.user ?: return@composable
-            ProfileScreen(
-                user             = user,
-                onSignOut        = {
-                    authVM.signOut()
-                    navController.navigate(Screen.Login.route) { popUpTo(0) { inclusive = true } }
-                },
+            ProfileScreen(user = user,
+                onSignOut = { authVM.signOut()
+                    navController.navigate(Screen.Login.route) { popUpTo(0) { inclusive = true } } },
                 onBuyCoins       = { navController.navigate(Screen.BuyCoins.route) },
                 onSubscribe      = { navController.navigate(Screen.Subscribe.route) },
                 onAdminDashboard = { navController.navigate(Screen.AdminDashboard.route) },
                 onBack           = { navController.switchTab(Screen.Home.route) },
                 onWriteClick     = { navController.switchTab(Screen.Write.route) },
-                onLibraryClick   = { navController.switchTab(Screen.Library.route) }
-            )
+                onLibraryClick   = { navController.switchTab(Screen.Library.route) })
         }
 
         composable(Screen.AdminDashboard.route) {
